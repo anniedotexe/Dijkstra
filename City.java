c908edbe4bce555f915069dba10e955ea7c91657
Annie Wu <anniewu201@gmail.com>
2018-05-20 13:53:50 -0700

    Update City.java

--- City.java
+++ City.java
@@ -3,7 +3,7 @@
  * Author:		Annie Wu
  * Class:		CS 241 - Data Structures and Algorithms II
  * 
- * Assignment:	Program 4
+ * Assignment:		Program 4
  * Date:		16 March 2018
  * 
  * Purpose:		This program contains the cities and any functions regarding the city information.
